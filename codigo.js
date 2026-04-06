932fb09f74b02ee89bc7bc752a789512121f8986
FELIPE MANUEL ANDRACA <48803376@ort>
2026-04-06 09:33:17 -0300

    js funcionalidad completa

--- codigo.js
+++ codigo.js
@@ -1,7 +1,17 @@
 const texto = document.getElementById("texto");
 const boton = document.getElementById("boton");
+const input = document.getElementById("input");
 
-boton.addEventListener("click", () => obtenerPokemon("ditto"));
+
+boton.addEventListener("click",() =>{
+    if (input.value != false) {
+        obtenerPokemon(input.value)
+    }
+    else
+    {
+        texto.innerText = "valor invalido"
+    }
+});
 
 async function obtenerPokemon(nombre) {
     try {
@@ -13,17 +23,16 @@ async function obtenerPokemon(nombre) {
         let img = data.sprites.front_default;
         let tipos = [];
         data.types.forEach(element => {
-            tipos.push(element.type.name)
+            tipos.push(element.type.name + " ")
         });
 
 
         texto.innerHTML = `
-        <h1>${nombre2}</h1>
+        <p>${nombre2}</p>
         <img src="${img}"/>
-        <p>${tipos}</p>
-        <p>peso: ${data.weight}</p>
+        <p>Tipos: ${tipos}</p>
+        <p>Peso: ${data.weight}</p>
         <p>Altura:${data.height}</p>
-
         `;
     } catch (error) {
        texto.innerText = error.message;
